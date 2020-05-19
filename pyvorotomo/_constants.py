dc944aaaa3fe52d6520427d5fd5b09064bb84c17
Malcolm White <malcolcw@usc.edu>
2020-05-19 10:39:40 -0700

    Add HDF5_PARALLEL_ENABLED to _constants module.

--- pyvorotomo/_constants.py
+++ pyvorotomo/_constants.py
@@ -8,12 +8,12 @@ DTYPE_INT                 = np.int64
 DTYPE_REAL                = np.float64
 
 EVENT_DTYPES = dict(
-    event_id=np.int64,
-    latitude=np.float64,
-    longitude=np.float64,
-    depth=np.float64,
-    time=np.float64,
-    residual=np.float64
+    event_id=DTYPE_INT,
+    latitude=DTYPE_REAL,
+    longitude=DTYPE_REAL,
+    depth=DTYPE_REAL,
+    time=DTYPE_REAL,
+    residual=DTYPE_REAL
 )
 EVENT_FIELDS = [
     "event_id",
@@ -25,12 +25,12 @@ EVENT_FIELDS = [
 ]
 
 ARRIVAL_DTYPES = dict(
-    event_id=np.int64,
+    event_id=DTYPE_INT,
     network=str,
     station=str,
     phase=str,
-    time=np.float64,
-    residual=np.float64
+    time=DTYPE_REAL,
+    residual=DTYPE_REAL
 )
 ARRIVAL_FIELDS = [
     "event_id",
@@ -40,3 +40,23 @@ ARRIVAL_FIELDS = [
     "time",
     "residual"
 ]
+try:
+
+    import h5py
+    import mpi4py.MPI as MPI
+    import os
+
+    COMM = MPI.COMM_WORLD
+
+    filename = ".MbCZm61Z3M"
+    f5 = h5py.File(filename, 'w', driver='mpio', comm=COMM)
+    f5.close()
+
+    if COMM.Get_rank() == ROOT_RANK:
+        os.remove(filename)
+
+    HDF5_PARALLEL_ENABLED = True
+
+except AttributeError:
+
+    HDF5_PARALLEL_ENABLED = False
